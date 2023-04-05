e12121622d78d11dde3c3237c1f3605a1533edf0
SeoMoonk <sunghoon1216@naver.com>
2023-04-05 15:10:44 +0900

    1Week_template_complete

--- src/main/java/com/ll/gramgram/base/initData/NotProd.java
+++ src/main/java/com/ll/gramgram/base/initData/NotProd.java
@@ -26,7 +26,9 @@ public class NotProd {
             Member memberUser4 = memberService.join("user4", "1234").getData();
             Member memberUser5 = memberService.join("user5", "1234").getData();
 
-            Member memberUser5ByKakao = memberService.whenSocialLogin("KAKAO", "KAKAO__2733173737").getData();
+            Member memberUser6ByKakao = memberService.whenSocialLogin("KAKAO", "KAKAO__2733173737").getData();
+            Member memberUser7ByGoogle = memberService.whenSocialLogin("GOOGLE", "GOOGLE__2154586215").getData();
+
 
             instaMemberService.connect(memberUser2, "insta_user2", "M");
             instaMemberService.connect(memberUser3, "insta_user3", "W");
