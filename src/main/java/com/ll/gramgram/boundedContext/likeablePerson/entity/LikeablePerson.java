1950e43fec5a1a235010eb5ff6e5f7d405f2b5fe
SeoMoonk <sunghoon1216@naver.com>
2023-04-25 11:42:34 +0900

    Front : Listpage upgrade

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/entity/LikeablePerson.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/entity/LikeablePerson.java
@@ -56,4 +56,12 @@ public class LikeablePerson {
         this.attractiveTypeCode = attractiveTypeCode;
         this.modifyDate = LocalDateTime.now();
     }
+
+    public String getAttractiveTypeDisplayNameWithIcon() {
+        return switch (attractiveTypeCode) {
+            case 1 -> "<i class=\"fa-solid fa-person-rays\"></i>";
+            case 2 -> "<i class=\"fa-regular fa-face-smile\"></i>";
+            default -> "<i class=\"fa-solid fa-people-roof\"></i>";
+        } + "&nbsp;" + getAttractiveTypeDisplayName();
+    }
 }
